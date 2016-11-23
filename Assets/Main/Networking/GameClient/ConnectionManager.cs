4425ec23305a481a44d681dcc5da58d61863e537
Huayu Yang <yhygoo@gmail.com>
2016-11-22 21:17:01 -0800

    singleton class

---
+++ Assets/Main/Networking/GameClient/ConnectionManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Owlies.Core {
+    public class ConnectionManager : Singleton<ConnectionManager> {
+        private int session;
+
+        void Start() {
+
+        }
+
+        void Initialize() {
+            
+        }
+        
+    }
+}
