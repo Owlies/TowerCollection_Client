95219a1b6050dc30b503700f122be9c896f62422
Yang Sun <yang.sun@crowdstar.com>
2016-12-05 21:42:32 -0800

    reset my local change, add meta file

--- Assets/Main/Networking/GameClient/SocketClient.cs
+++ Assets/Main/Networking/GameClient/SocketClient.cs
@@ -148,7 +148,6 @@ namespace GameSocket
                 {
                     byte[] array = (byte[])iar.AsyncState;
 
-					Debug.Log("keep connect checking : " + num + " : " + array.Length);
                     if (this.SocketMessageReceivedFromServer != null && array != null)
                     {
                         this.SocketMessageReceivedFromServer(this, new SocketMessageReceivedFromServer(array, num));
@@ -157,10 +156,6 @@ namespace GameSocket
                     this.receivedDataSize = array[0] * 256 + array[1];
                     this.receivedData = new byte[num - 2];
                     System.Buffer.BlockCopy(array, 2, this.receivedData, 0, num - 2);
-					for(int i = 0;i<num;i++)
-					{
-						Debug.Log((int)this.receivedData[i]); 
-					}
 
                     array = new byte[bufferSize];
                     this.connection.BeginReceive(array, 0, bufferSize, SocketFlags.None, new AsyncCallback(this.KeepConnect), array);
